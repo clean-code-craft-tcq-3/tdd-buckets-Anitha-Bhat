9a68e3e49c8a7ac77b47f7a84a34fd6ba1e9ef6b
Anitha-Bhat <anithabhat97@gmail.com>
2022-06-13 00:55:08 +0530

    Update RangeReader.cs

--- RangeReaderProgram/RangeReader.cs
+++ RangeReaderProgram/RangeReader.cs
@@ -1,37 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using NUnit.Framework.Constraints;
 
 namespace RangeReader
 {
-  public interface IRangeReader
-  {
-    void HandleRangeReader(List<int> sortedSamples, int rangeFrom, int rangeTo, Action<int, int, int> printFunction);
-    int GetNumberOfReading(List<int> sortedSamples, int rangeFrom, int rangeTo);
-    void PrintRangeData(int rangeFrom, int rangeTo, int ReaderCount);
 
-  }
-  public class RangeReader: IRangeReader
+  public class RangeReader
   {
+    [ExcludeFromCodeCoverage]
     public static void Main(string[] args)
     {
+      List<int> sampleList = new List<int> { 3, 3, 5, 4, 10, 11, 12 };
+
+      HandleRangeReader(sampleList, 3, 10, PrintRangeData);
 
     }
-
-    public void HandleRangeReader(List<int> sortedSamples,int rangeFrom,int rangeTo,Action<int,int,int> printFunction)
+    public static void HandleRangeReader(List<int> Samples,int rangeFrom,int rangeTo,Action<int,int,int> printFunction)
     {
-      throw new Exception("Not implemented");
+      int numberOfReading=GetNumberOfReading(Samples, rangeFrom, rangeTo);
+      printFunction(rangeFrom, rangeTo, numberOfReading);
     }
 
-    public  int GetNumberOfReading(List<int> sortedSamples, int rangeFrom, int rangeTo)
+    public static int GetNumberOfReading(List<int> Samples, int rangeFrom, int rangeTo)
     {
-     throw new Exception("Not implemented");
+      if (Samples == null)
+      {
+        throw new NullReferenceException("value cannot be null");
+      }
+      Samples= Samples.Distinct().OrderBy(item => item).ToList();
+      int readingCount = Samples.Where(x => x >= rangeFrom && x <= rangeTo).Count();
+     return readingCount;
     }
     
 
-    public  void PrintRangeData(int rangeFrom,int rangeTo,int ReaderCount)
+    public static void PrintRangeData(int rangeFrom,int rangeTo,int ReaderCount)
     {
-      throw new Exception("Not implemented");
+      Console.WriteLine("Range {0}-{1},Reading {2}",rangeFrom,rangeTo,ReaderCount);
+    
     }
 
   }
