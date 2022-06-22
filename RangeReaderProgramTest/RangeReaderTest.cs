b255855e55c29c98c157e705fccbc29551dbdbaa
Anitha-Bhat <anithabhat97@gmail.com>
2022-06-23 00:15:32 +0530

    Update RangeReaderTest.cs

--- RangeReaderProgramTest/RangeReaderTest.cs
+++ RangeReaderProgramTest/RangeReaderTest.cs
@@ -10,43 +10,44 @@ namespace RangeReader
 
   public class RangeReaderTest
   {
-      List<int> sampleList =new List<int> { 2, 3, 4, 5, 5, 6, 8, 9, 10, 11, 12 };
-      List<string> expectedRange=new List<string>{"(2-6),6","(8-12),5"};
+    List<int> sampleList = new List<int> { 2, 3, 4, 5, 5, 6, 8, 9, 10, 11, 12 };
+    List<string> expectedRange = new List<string> { "(2-6),6", "(8-12),5" };
     [Test]
     public void getNumberOfReadingTest()
     {
-      
+
       var numberOfReading = RangeReader.GetConsecutiveRangeReadings(sampleList);
       Assert.AreEqual(numberOfReading, expectedRange);
-       List<int> sampleList1 =new List<int> { -1, 3, 4, 5, 20, 21, 25 };
-  
-      List<string> expectedRange1=new List<string>{"(3-5),3","(20-21),2"};
-       var numberOfReading = RangeReader.GetConsecutiveRangeReadings(sampleList1);
-      Assert.AreEqual(numberOfReading, expectedRange1);
-        List<int> sampleList2 =new List<int> { -1, 0, 1, 2, 5, 6, 8 };
-  
-      List<string> expectedRange2=new List<string>{"(-1-2),4","(5-6),2"};
-       var numberOfReading = RangeReader.GetConsecutiveRangeReadings(sampleList2);
-      Assert.AreEqual(numberOfReading, expectedRange2);
+      List<int> sampleList1 = new List<int> { -1, 3, 4, 5, 20, 21, 25 };
+
+      List<string> expectedRange1 = new List<string> { "(3-5),3", "(20-21),2" };
+      var numberOfReading1 = RangeReader.GetConsecutiveRangeReadings(sampleList1);
+      Assert.AreEqual(numberOfReading1, expectedRange1);
+      List<int> sampleList2 = new List<int> { -1, 0, 1, 2, 5, 6, 8 };
+
+      List<string> expectedRange2 = new List<string> { "(-1-2),4", "(5-6),2" };
+      var numberOfReading2 = RangeReader.GetConsecutiveRangeReadings(sampleList2);
+      Assert.AreEqual(numberOfReading2, expectedRange2);
     }
 
-   
-    public void getNumberOfReadingExceptionTest(List<int> sampleList)
+
+    public void getNumberOfReadingExceptionTest()
     {
-     
-        var ex = Assert.Throws<NullReferenceException>(() => RangeReader.HandleRangeReader(new List<int>{}));
- 
-     
+
+      var ex = Assert.Throws<NullReferenceException>(() =>
+        RangeReader.HandleRangeReader(new List<int> { }, Substitute.For<Action<List<string>>>()));
+
+
     }
     [Test]
     public void HandleRangeReader()
     {
-     
+
       Action<List<string>> printFunction = Substitute.For<Action<List<string>>>();
-      RangeReader.HandleRangeReader(sampleList,printFunction);
+      RangeReader.HandleRangeReader(sampleList, printFunction);
       printFunction.Received(1);
-      
-     
+
+
 
     }
   }
