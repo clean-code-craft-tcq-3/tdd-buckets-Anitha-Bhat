52477e7d9d38a78548051ea4aef7a1f9c93f1deb
Anitha-Bhat <anithabhat97@gmail.com>
2022-06-22 21:47:34 +0530

    Update RangeReader.cs
    updated the code

--- RangeReaderProgram/RangeReader.cs
+++ RangeReaderProgram/RangeReader.cs
@@ -2,43 +2,83 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using NUnit.Framework.Constraints;
 
 namespace RangeReader
 {
 
   public class RangeReader
   {
+    private static bool isLastCompare(int sampleCount, int currentIndex)
+    {
+      return sampleCount - 1 == currentIndex + 1;
+    }
+
     [ExcludeFromCodeCoverage]
     public static void Main(string[] args)
     {
-      List<int> sampleList = new List<int> { 3, 3, 5, 4, 10, 11, 12 };
+      List<int> sampleList = new List<int>
+      { 2, 3, 4, 5, 5, 6, 8, 9, 10, 11, 12 };
 
-      HandleRangeReader(sampleList, 3, 10, PrintRangeData);
+      HandleRangeReader(sampleList, PrintRangeData);
 
     }
-    public static void HandleRangeReader(List<int> Samples,int rangeFrom,int rangeTo,Action<int,int,int> printFunction)
+    public static void HandleRangeReader(List<int> Samples, Action<List<string>> printFunction)
     {
-      int numberOfReading=GetNumberOfReading(Samples, rangeFrom, rangeTo);
-      printFunction(rangeFrom, rangeTo, numberOfReading);
+      List<string> rangeList = GetConsecutiveRangeReadings(Samples);
+      printFunction(rangeList);
     }
 
-    public static int GetNumberOfReading(List<int> Samples, int rangeFrom, int rangeTo)
+    public static List<string> GetConsecutiveRangeReadings(List<int> Samples)
     {
+
       if (Samples == null)
       {
         throw new NullReferenceException("value cannot be null");
       }
-      Samples= Samples.OrderBy(item => item).ToList();
-      int readingCount = Samples.Where(x => x >= rangeFrom && x <= rangeTo).Count();
-     return readingCount;
-    }
-    
+      List<string> groupRangeList = new List<string>();
+      Samples = Samples.OrderBy(item => item).ToList();
+      int consecutiveNumTracker = 0;
+      for (int i = 0; i < Samples.Count-1; i++)
+      {
+       
+        bool isConsecutive = isConsecutiveNumber(Samples[i], Samples[i + 1]);
+        if (isConsecutive)
+          consecutiveNumTracker++;
+        
+        bool checkIfLastCompare = isLastCompare(Samples.Count, i);
+        if (isConsecutive && !checkIfLastCompare)
+           continue;
 
-    public static void PrintRangeData(int rangeFrom,int rangeTo,int ReaderCount)
+        if (consecutiveNumTracker > 0)
+        {
+          groupRangeList.Add(string.Format("({0}-{1}),{2}", 
+            isConsecutive && checkIfLastCompare?Samples[i+1 - consecutiveNumTracker] : Samples[i -consecutiveNumTracker]
+                             , 
+            isConsecutive && checkIfLastCompare ? Samples[i+1]: Samples[i], consecutiveNumTracker+1));
+          consecutiveNumTracker = 0;
+        }
+      
+
+      }
+
+      return groupRangeList;
+    }
+
+    private static bool isConsecutiveNumber(int currentNumber, int nextNumber)
     {
-      Console.WriteLine("Range {0}-{1},Reading {2}",rangeFrom,rangeTo,ReaderCount);
-    
+      if (nextNumber - currentNumber == 1 || nextNumber - currentNumber == 0)
+      {
+        return true;
+      }
+
+      return false;
+
+    }
+    public static void PrintRangeData(List<string> range)
+    {
+      range.ForEach(r => Console.WriteLine($"{r}\n"));
+
+
     }
 
   }
